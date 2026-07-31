41b20af4d6451a7d690a1ad68e87d687dddfdf72
Kavya Katal <kavyakatal09@gmail.com>
2026-07-31 19:40:25 +0530

    .

--- index.js
+++ index.js
@@ -2,5 +2,3 @@ app.get("/kavya",(req,res)=>{
 
 
 })
-
-// This is a simple Express.js route handler for the GET request to the "/kavya" endpoint. When a client makes a GET request to this endpoint, the callback function will be executed. However, currently, the callback function is empty and does not send any response back to the client.
